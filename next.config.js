67cfae747efc07fb246ef9d586d6a6b45cdc1ff0
duzenebru01 <duzenebru01@users.noreply.github.com>
2025-06-15 00:28:27 +0300

    fix: next.config.js'den gereksiz experimental.serverActions ayarı kaldırıldı

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   output: 'standalone',
   assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '',
 };
