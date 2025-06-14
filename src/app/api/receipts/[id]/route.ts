8b5a2b9db472abae46bd0f06548e484feeb6a1c7
duzenebru01 <duzenebru01@users.noreply.github.com>
2025-06-14 21:57:54 +0300

    Fix ESLint errors and update user configuration

--- src/app/api/receipts/[id]/route.ts
+++ src/app/api/receipts/[id]/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { clientPromise } from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
 
 export async function GET(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   try {
